1635aa954127c2e003d5aa15696a7c3034923e8d
Michael Cruz <mrcruz1989@gmail.com>
2021-06-23 10:43:38 -0400

    finished 3LargestSums

---
+++ algoExpert/easy/3LargestNums.js
@@ -0,0 +1,20 @@
+function findThreeLargestNumbers(array) {
+  const largestNums = [null, null, null];
+  largestNums.sort((a, b) => return a - b);
+  for (const num of array) {
+    if (num > largestNums[2] || largestNums[2] === null) {
+      if (largestNums[2]) {
+        var temp = largestNums[1];
+        largestNums[1] = largestNums[2];
+        largestNums[0] = temp;
+      }
+      largestNums[2] = num;
+    } else if (num > largestNums[1] || largestNums[1] === null) {
+      if (largestNums[1]) largestNums[0] = largestNums[1];
+      largestNums[1] = num;
+    } else if (num > largestNums[0] || largestNums[0] === null) {
+      largestNums[0] = num;
+    }
+  }
+  return largestNums;
+}
