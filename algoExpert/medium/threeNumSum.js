b7475710234046076e842830eacb5fb612a5b814
Michael Cruz <mrcruz1989@gmail.com>
2021-06-26 16:22:53 -0400

    completed 3 number sum

---
+++ algoExpert/medium/threeNumSum.js
@@ -0,0 +1,22 @@
+function threeNumberSum(array, targetSum) {
+  array.sort((a, b) => a - b);
+  const result = [];
+  for (let i = 0; i < array.length - 2; i++) {
+    let left = i + 1;
+    let right = array.length - 1;
+    while (left < right) {
+      const currentSum = array[i] + array[left] + array[right];
+      if (currentSum === targetSum) {
+        result.push([array[i], array[left], array[right]]);
+        left++;
+        right--;
+      } else if (currentSum < targetSum) {
+        left++;
+      } else if (currentSum > targetSum) {
+        right--;
+      }
+    }
+    endNumIdx = array.length - 1;
+  }
+  return result;
+}
