516416f09d1bb0e261a79c8d019d7cd2ad060646
Michael Cruz <mrcruz1989@gmail.com>
2021-07-06 21:36:12 -0400

    completed invert bst

---
+++ algoExpert/medium/invertBst.js
@@ -0,0 +1,22 @@
+function invertBinaryTree(tree) {
+  if (tree !== null) {
+    swapNodes(tree);
+    invertBinaryTree(tree.left);
+    invertBinaryTree(tree.right);
+  }
+  // return tree
+}
+
+const swapNodes = (tree) => {
+  const left = tree.left;
+  tree.left = tree.right;
+  tree.right = left;
+};
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
