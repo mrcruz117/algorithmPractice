5f6011c445e6c399798511b690bebb9da832768d
Michael Cruz <mrcruz1989@gmail.com>
2021-07-19 11:16:35 -0600

    completed number of ways to make change

---
+++ algoExpert/medium/waysToMakeChange.js
@@ -0,0 +1,15 @@
+function numberOfWaysToMakeChange(n, denoms) {
+  if (n === 0) return 1;
+  let counter = 0;
+  const ways = new Array(n + 1).fill(0);
+  ways[0] = 1;
+  for (let denom of denoms) {
+    for (let amount = 1; amount < n + 1; amount++) {
+      if (denom <= amount) {
+        ways[amount] += ways[amount - denom];
+      }
+    }
+  }
+  console.log(ways);
+  return ways[n];
+}
